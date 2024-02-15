98f46253a85ec4ffe75c8f7fb113331d5b74b70e
Thi <nguyenthisc1@gmail.com>
2024-02-15 14:54:54 +0700

    build circular container

---
+++ lib/common/widgets/custom_shapes/containers/primary_header_container.dart
@@ -0,0 +1,45 @@
+import 'package:ecommerce/common/widgets/custom_shapes/containers/circular_container.dart';
+import 'package:ecommerce/common/widgets/custom_shapes/cuved_edges/cuved_edeges_widget.dart';
+import 'package:ecommerce/utils/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+class TPrimaryHeaderContainer extends StatelessWidget {
+  const TPrimaryHeaderContainer({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return TCurvedEdgesWidget(
+      child: Container(
+        color: TColors.primary,
+        padding: const EdgeInsets.all(0),
+        child: SizedBox(
+          height: 400,
+          child: Stack(
+            children: [
+              Positioned(
+                top: -150,
+                right: -250,
+                child: TCircularContainer(
+                  backgroundColor: TColors.textWhite.withOpacity(0.1),
+                ),
+              ),
+              Positioned(
+                top: 100,
+                right: -300,
+                child: TCircularContainer(
+                  backgroundColor: TColors.textWhite.withOpacity(0.1),
+                ),
+              ),
+              child
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
