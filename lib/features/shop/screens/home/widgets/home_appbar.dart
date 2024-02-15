ba8aa187f8b5324ddce8b261099d48dbabc6357a
Thi <nguyenthisc1@gmail.com>
2024-02-15 17:04:00 +0700

    feat: build home appbar screen

---
+++ lib/features/shop/screens/home/widgets/home_appbar.dart
@@ -0,0 +1,40 @@
+import 'package:ecommerce/common/widgets/appbar/appbar.dart';
+import 'package:ecommerce/common/widgets/appbar/products/cart/cart_menu_icon.dart';
+import 'package:ecommerce/utils/constants/text_strings.dart';
+import 'package:flutter/material.dart';
+
+class THomeAppbar extends StatelessWidget {
+  const THomeAppbar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TAppbar(
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            TTexts.homeAppbarTitle,
+            style: Theme.of(context)
+                .textTheme
+                .labelMedium!
+                .apply(color: Colors.grey),
+          ),
+          Text(
+            TTexts.homeAppbarSubTitle,
+            style: Theme.of(context)
+                .textTheme
+                .headlineSmall!
+                .apply(color: Colors.white),
+          )
+        ],
+      ),
+      actions: [
+        TCartCounterIcon(
+          onPressed: () {},
+        )
+      ],
+    );
+  }
+}
