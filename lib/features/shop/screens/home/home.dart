0ed87a45cc417ef4dbbece69e88dc89dc89e89b7
Thi <nguyenthisc1@gmail.com>
2024-02-16 16:30:03 +0700

    feat: build home slider

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,5 +1,14 @@
 import 'package:ecommerce/common/widgets/custom_shapes/containers/primary_header_container.dart';
+import 'package:ecommerce/common/widgets/custom_shapes/containers/search_container.dart';
+import 'package:ecommerce/common/widgets/texts/section_heading.dart';
+import 'package:ecommerce/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:ecommerce/features/shop/screens/home/widgets/home_categories.dart';
+import 'package:ecommerce/features/shop/screens/home/widgets/home_slider.dart';
+import 'package:ecommerce/utils/constants/colors.dart';
+import 'package:ecommerce/utils/constants/image_strings.dart';
+import 'package:ecommerce/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,8 +20,51 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           children: [
             TPrimaryHeaderContainer(
-              child: Column(children: []),
+              child: Column(children: [
+                // APPBAR
+                THomeAppbar(),
+
+                SizedBox(
+                  height: TSizes.spaceBtwSections,
+                ),
+
+                // SEARCH BAR
+                TSearchContainer(
+                  text: 'Search in Store',
+                  icon: Iconsax.search_normal,
+                ),
+
+                SizedBox(
+                  height: TSizes.spaceBtwSections,
+                ),
+
+                // CATEGORIES
+                TSearchHeading(
+                  title: 'Popular Categories',
+                  buttonTitle: "Button",
+                  showActionButton: false,
+                  textColor: TColors.white,
+                ),
+
+                SizedBox(
+                  height: TSizes.spaceBtwItems,
+                ),
+
+                // CATEGORIES
+                THomeCategories()
+              ]),
             ),
+
+            // BODY
+            Padding(
+                padding: EdgeInsets.all(TSizes.defaultSpace),
+                child: TPromoSlider(
+                  banners: [
+                    TImages.promoBanner1,
+                    TImages.promoBanner2,
+                    TImages.promoBanner3,
+                  ],
+                ))
           ],
         ),
       ),
---
+++ lib/features/shop/screens/home/widgets/home_slider.dart
@@ -0,0 +1,62 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:ecommerce/common/widgets/custom_shapes/containers/circular_container.dart';
+import 'package:ecommerce/common/widgets/image/rounded_image.dart';
+import 'package:ecommerce/features/shop/controllers/home_controller.dart';
+import 'package:ecommerce/utils/constants/colors.dart';
+import 'package:ecommerce/utils/constants/image_strings.dart';
+import 'package:ecommerce/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class TPromoSlider extends StatelessWidget {
+  const TPromoSlider({
+    super.key,
+    required this.banners,
+  });
+
+  final List<String> banners;
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(HomeController());
+    return Column(
+      children: [
+        CarouselSlider(
+            items: banners
+                .map(
+                  (url) => TRoundedImage(
+                    imageUrl: url,
+                  ),
+                )
+                .toList(),
+            options: CarouselOptions(
+                viewportFraction: 1,
+                onPageChanged: (index, _) =>
+                    controller.UpdatePageIndicator(index))),
+        const SizedBox(
+          height: TSizes.spaceBtwItems,
+        ),
+
+        // CIRCULAR
+        Center(
+          child: Obx(
+            () => Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                for (int i = 0; i < banners.length; i++)
+                  TCircularContainer(
+                      width: 20,
+                      height: 5,
+                      margin: const EdgeInsets.only(right: 10),
+                      backgroundColor:
+                          controller.carousalCurrentIndex.value == i
+                              ? TColors.primary
+                              : TColors.grey),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
