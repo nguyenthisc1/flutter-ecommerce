a9620defea9d9cfae12623c598674b46e584073f
Thi <nguyenthisc1@gmail.com>
2024-02-18 20:07:29 +0700

    feat: build brand showcase

---
+++ lib/common/widgets/brands/brand_show_case.dart
@@ -0,0 +1,55 @@
+
+import 'package:ecommerce/common/widgets/custom_shapes/containers/rounded_container.dart';
+import 'package:ecommerce/common/widgets/brands/brand_card.dart';
+import 'package:ecommerce/utils/constants/colors.dart';
+import 'package:ecommerce/utils/constants/sizes.dart';
+import 'package:ecommerce/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+
+class TBrandShowcase extends StatelessWidget {
+  const TBrandShowcase({
+    super.key,
+    required this.images,
+  });
+
+  final List<String> images;
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+
+    return TRoundedContainer(
+      showBorder: true,
+      borderColor: TColors.darkGrey,
+      backgroundColor: Colors.transparent,
+      padding: const EdgeInsets.all(TSizes.md),
+      margin: const EdgeInsets.only(bottom: TSizes.spaceBtwItems),
+      child: Column(children: [
+        // BRAND WITH PRODUCTS COUNT
+        const TBrandCard(showBorder: false),
+        // BRAND TOP 3 PRODUCT IMAGE
+        Row(
+          children: images
+              .map((image) => BrandTopProductImageWidget(image, context))
+              .toList(),
+        )
+      ]),
+    );
+  }
+
+  Widget BrandTopProductImageWidget(String image, context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return Expanded(
+      child: TRoundedContainer(
+        height: 100,
+        backgroundColor: dark ? TColors.darkGrey : TColors.light,
+        margin: const EdgeInsets.only(right: TSizes.sm),
+        padding: const EdgeInsets.all(TSizes.md),
+        child: Image(
+          fit: BoxFit.contain,
+          image: AssetImage(image),
+        ),
+      ),
+    );
+  }
+}
