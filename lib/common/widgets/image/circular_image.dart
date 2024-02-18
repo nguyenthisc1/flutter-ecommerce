9aeda6c7ab8d4b7746269af236fe04a09c51c742
Thi <nguyenthisc1@gmail.com>
2024-02-18 20:06:45 +0700

    feat: build circular image components

---
+++ lib/common/widgets/image/circular_image.dart
@@ -0,0 +1,45 @@
+import 'package:ecommerce/utils/constants/colors.dart';
+import 'package:ecommerce/utils/constants/image_strings.dart';
+import 'package:ecommerce/utils/constants/sizes.dart';
+import 'package:ecommerce/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+
+class TCircularImage extends StatelessWidget {
+  const TCircularImage({
+    super.key,
+    this.width = 56,
+    this.height = 56,
+    this.padding = TSizes.sm,
+    this.fit = BoxFit.cover,
+    required this.image,
+    this.isNetworkImage = false,
+    this.backgroundColor,
+    this.overlayColor,
+  });
+
+  final double width, height, padding;
+  final BoxFit fit;
+  final String image;
+  final bool isNetworkImage;
+  final Color? backgroundColor, overlayColor;
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return Container(
+      width: width,
+      height: height,
+      padding: EdgeInsets.all(padding),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100),
+          color: dark ? TColors.black : TColors.white),
+      child: Image(
+        fit: fit,
+        image: isNetworkImage
+            ? NetworkImage(image)
+            : AssetImage(image) as ImageProvider,
+        color: overlayColor,
+      ),
+    );
+  }
+}
