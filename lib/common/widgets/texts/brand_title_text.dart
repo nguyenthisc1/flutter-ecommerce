c8873a577efb710b40e0fab43f0ce28e3bc59788
Thi <nguyenthisc1@gmail.com>
2024-02-18 20:07:42 +0700

    feat: build brand title text

---
+++ lib/common/widgets/texts/brand_title_text.dart
@@ -0,0 +1,39 @@
+import 'package:ecommerce/utils/constants/colors.dart';
+import 'package:ecommerce/utils/constants/enums.dart';
+import 'package:ecommerce/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class TBrandTitleText extends StatelessWidget {
+  const TBrandTitleText({
+    super.key,
+    required this.title,
+    this.maxLines = 1,
+    this.color,
+    this.textAlign = TextAlign.center,
+    this.brandTextSizes = TextSizes.small,
+  });
+
+  final String title;
+  final int maxLines;
+  final Color? color;
+  final TextAlign textAlign;
+  final TextSizes brandTextSizes;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      overflow: TextOverflow.ellipsis,
+      textAlign: textAlign,
+      maxLines: maxLines,
+      style: brandTextSizes == TextSizes.small
+          ? Theme.of(context).textTheme.labelMedium!.apply(color: color)
+          : brandTextSizes == TextSizes.medium
+              ? Theme.of(context).textTheme.bodyLarge!.apply(color: color)
+              : brandTextSizes == TextSizes.large
+                  ? Theme.of(context).textTheme.titleLarge!.apply(color: color)
+                  : Theme.of(context).textTheme.bodyMedium!.apply(color: color),
+    );
+  }
+}
