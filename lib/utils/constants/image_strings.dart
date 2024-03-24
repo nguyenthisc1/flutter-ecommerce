b27865710a8c2357b1f07595394287a9bcb62582
Thi <nguyenthisc1@gmail.com>
2024-03-24 21:21:11 +0700

    assets: add lottie animation in to tImages class

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -3,7 +3,21 @@
 
 /// This class contains all the App Images in String formats.
 class TImages {
+  // Lottie Animations
   static const String docerAnimation = "assets/images/animations/141594-animation-of-docer.json";
+  static const String cartAnimation = "assets/images/animations/lady-adding-product-in-cart-animation.json";
+  static const String orderCompletedAnimation = "assets/images/animations/order-complete-car-delivery-animation.json";
+  static const String packageAnimation = "assets/images/animations/98783-packaging-in-progress.json";
+  static const String paperPlaneAnimation = "assets/images/animations/110052-paper-plane.json";
+  static const String searchAnimation = "assets/images/animations/72785-searching.json";
+  static const String emptyAnimation = "assets/images/animations/53207-empty-file.json";
+  static const String securityAnimation = "assets/images/animations/67263-security-icon-transparent.json";
+  static const String successfullyRegisterAnimation = "assets/images/animations/72462-check-register.json";
+  static const String thankYouAnimation = "assets/images/animations/104368-thank-you.json";
+  static const String paymentSuccessfulAnimation = "assets/images/animations/120978-payment-successful.json";
+  static const String loadingJuggleAnimation = "assets/images/animations/141397-loading-juggle.json";
+  static const String cloudUploadingAnimation = "assets/images/animations/cloud-uploading-animation.json";
+  static const String loaderAnimation = "assets/images/animations/loader-animation.json";
 
   // -- App Logos
   static const String darkAppLogo = "assets/logos/t-store-splash-logo-black.png";
